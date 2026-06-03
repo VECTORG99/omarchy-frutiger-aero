5db128f3880aab138a3aba4926d34b6cabc8d1e3
Diego Hernandez <dh8939693@gmail.com>
2026-06-02 21:02:03 -0400

    Fix clock locale+perms

--- config/waybar/scripts/clock.sh
+++ config/waybar/scripts/clock.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-LC_ALL=en_US.UTF-8
-ampm() {
-  local h
-  h=$(date +%H)
-  if (( h < 12 )); then echo "AM"; else echo "PM"; fi
-}
+export LC_ALL=en_US.utf8
 
 case ${1:-horizontal} in
   horizontal)
-    d=$(date "+%A %d/%m %I:%M")
-    echo "{\"text\": \"$d $(ampm)\", \"tooltip\": \"$(date)\", \"class\": \"\"}"
+    d=$(date "+%A %d/%m %I:%M %p")
+    echo "{\"text\": \"$d\", \"tooltip\": \"$(date)\", \"class\": \"\"}"
     ;;
   vertical)
-    d=$(date "+%A")
-    m=$(date "+%d/%m")
-    t=$(date "+%I:%M")
-    echo "{\"text\": \"${d}\n${m}\n${t}\n$(ampm)\", \"tooltip\": \"$(date)\", \"class\": \"\"}"
+    echo "{\"text\": \"$(date '+%A\n%d/%m\n%I:%M\n%p')\", \"tooltip\": \"$(date)\", \"class\": \"\"}"
     ;;
 esac
