80ea6ad79d113fb3d6355c6186f4652fac87943f
Diego Hernandez <dh8939693@gmail.com>
2026-06-13 18:33:40 -0400

    feat(clock): Frutiger Aero digital clock - 12h, dark glass, teal accents
    Dark theme FA clock with 12h format AM/PM, GMT timezone, seconds, glossy separators, teal-green accent bar, glass panel and reflections

--- eww/scripts/clock.sh
+++ eww/scripts/clock.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-LC_TIME=en_US.UTF-8 date +'{"hour":"%H","minute":"%M","second":"%S","ampm":"%p","weekday":"%a","day":"%d","month":"%m","moname":"%b"}'
+LC_TIME=en_US.UTF-8 date +'{"hour":"%I","minute":"%M","second":"%S","ampm":"%p","weekday":"%a","day":"%d","month":"%b","year":"%Y","tz":"%Z"}'
